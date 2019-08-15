30f8e748b2232ca9b1eb711eb1209e572906f150
tjmtmmnk <tsujimoto027@gmail.com>
2019-08-16 02:49:50 +0900

    2個以上の改行があるときは改行するようにした

--- trimmer.py
+++ trimmer.py
@@ -1,15 +1,19 @@
 from nltk.tokenize import sent_tokenize
 from typing import List
+import re
 
 
-def trim(raw_body) -> str:
-    token_list = _sharp_end_of_sentence(raw_body)
+def trim(raw_body: str) -> str:
+    new_line_shape = re.sub(r'(\n{2,})|((\r\n){2,})', "[newline]", raw_body)
+
+    token_list = _shape_end_of_sentence(new_line_shape)
     ret = ""
     for t in token_list:
-        ret += t + "\n"
+        ret += t.replace("[newline]", "\n") + "\n"
 
+    print(ret)
     return ret
 
 
-def _sharp_end_of_sentence(body: str) -> List[str]:
+def _shape_end_of_sentence(body: str) -> List[str]:
     return sent_tokenize(body)
