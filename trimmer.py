60251d57fb0eeed0bfadbc69ad3fbf7e7d45f319
tjmtmmnk <tsujimoto027@gmail.com>
2019-08-20 00:30:43 +0900

    関数に分けるようにリファクタリング

--- trimmer.py
+++ trimmer.py
@@ -4,24 +4,49 @@ import re
 
 
 def trim(raw_body: str) -> str:
-    new_line_shape = re.sub(r'(\n{2,})|((\r\n){2,})', "[newline]", raw_body)
-    white_space_shape = re.sub(r'( {2,})|((\n|\r\n) {1,})', '[whitespace]', new_line_shape)
+    # sent_tokenizeにかけると改行や空白が消えてしまうので先にフラグを立てておいて後で処理する
+    flagged_body = _create_new_line_flag(raw_body)
+    flagged_body = _create_white_space_flag(flagged_body)
 
-    token_list = _shape_end_of_sentence(white_space_shape)
-    ret = ""
-    for t in token_list:
-        trimmed_sentence = t
+    sentences = _shape_end_of_sentence(flagged_body)
 
-        # 先頭から見たいのでmatchを使う
-        if re.match(r'^\[whitespace\]', t):
-            trimmed_sentence = re.sub(r'^\[whitespace\]', '', t)
+    shaped_body = ""
+    for s in sentences:
+        sentence = _new_line(s)
+        sentence = _white_space(sentence)
 
-        trimmed_sentence = trimmed_sentence.replace("[newline]", "\n")
-        trimmed_sentence = trimmed_sentence.replace("[whitespace]", " ")
+        shaped_body += sentence
 
-        ret += trimmed_sentence + "\n"
+    return shaped_body
 
-    return ret
+
+def _create_new_line_flag(body: str) -> str:
+    return re.sub(r'(\n{2,})|((\r\n){2,})', "[newline]", body)
+
+
+def _new_line(sentence: str) -> str:
+    if re.search(r"[newline]", sentence):
+        shaped_sentence = sentence.replace("[newline]", "\n\n")
+    else:
+        shaped_sentence = sentence + "\n"
+
+    return shaped_sentence
+
+
+def _white_space(sentence: str) -> str:
+    shaped_sentence = sentence
+
+    # 先頭の空白は消す
+    if re.match(r'^\[whitespace\]', sentence):
+        shaped_sentence = re.sub(r'^\[whitespace\]', '', sentence)
+
+    shaped_sentence = shaped_sentence.replace("[whitespace]", " ")
+
+    return shaped_sentence
+
+
+def _create_white_space_flag(body: str) -> str:
+    return re.sub(r'( {2,})|((\n|\r\n) {1,})', '[whitespace]', body)
 
 
 def _shape_end_of_sentence(body: str) -> List[str]:
