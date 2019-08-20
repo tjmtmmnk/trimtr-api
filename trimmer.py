e5500eb0063dae636003578d4a913bd0238200de
tjmtmmnk <tsujimoto027@gmail.com>
2019-08-20 16:33:14 +0900

    文中に改行があったときに表示が乱れていた

--- trimmer.py
+++ trimmer.py
@@ -8,12 +8,12 @@ def trim(raw_body: str) -> str:
     flagged_body = _create_new_line_flag(raw_body)
     flagged_body = _create_white_space_flag(flagged_body)
 
-    sentences = _shape_end_of_sentence(flagged_body)
+    sentences = _split_body_to_sentences(flagged_body)
 
     shaped_body = ""
     for s in sentences:
-        sentence = _new_line(s)
-        sentence = _white_space(sentence)
+        sentence = _shape_white_space(s)
+        sentence = _shape_new_line(sentence)
 
         shaped_body += sentence
 
@@ -24,16 +24,19 @@ def _create_new_line_flag(body: str) -> str:
     return re.sub(r'(\n{2,})|((\r\n){2,})', "[newline]", body)
 
 
-def _new_line(sentence: str) -> str:
-    if re.search(r"[newline]", sentence):
-        shaped_sentence = sentence.replace("[newline]", "\n\n")
+def _shape_new_line(sentence: str) -> str:
+    # [newline] を \n に置換する前に \n を空白に変換する
+    shaped_sentence = _new_line_to_white_space(sentence)
+
+    if re.search(r"\[newline\]", sentence):
+        shaped_sentence = shaped_sentence.replace("[newline]", "\n\n")
     else:
-        shaped_sentence = sentence + "\n"
+        shaped_sentence = shaped_sentence + "\n"
 
     return shaped_sentence
 
 
-def _white_space(sentence: str) -> str:
+def _shape_white_space(sentence: str) -> str:
     shaped_sentence = sentence
 
     # 先頭の空白は消す
@@ -45,9 +48,13 @@ def _white_space(sentence: str) -> str:
     return shaped_sentence
 
 
+def _new_line_to_white_space(sentence: str) -> str:
+    return sentence.replace("\n", " ")
+
+
 def _create_white_space_flag(body: str) -> str:
     return re.sub(r'( {2,})|((\n|\r\n) {1,})', '[whitespace]', body)
 
 
-def _shape_end_of_sentence(body: str) -> List[str]:
+def _split_body_to_sentences(body: str) -> List[str]:
     return sent_tokenize(body)
