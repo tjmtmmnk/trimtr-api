73fd47a57192d3743b441b53db83fdee85bb0946
tjmtmmnk <tsujimoto027@gmail.com>
2019-09-04 00:15:34 +0900

    NLとSBを分けていなくて無駄に改行が発生していた

--- trimmer.py
+++ trimmer.py
@@ -5,7 +5,7 @@ import re
 
 def trim(raw_body: str) -> str:
     # sent_tokenizeにかけると改行や空白が消えてしまうので先にフラグを立てておいて後で処理する
-    flagged_body = _create_new_line_flag(raw_body)
+    flagged_body = _create_sentence_block_flag(raw_body)
     flagged_body = _create_white_space_flag(flagged_body)
     flagged_body = _create_colon_flag(flagged_body)
 
@@ -18,11 +18,13 @@ def trim(raw_body: str) -> str:
 
         shaped_body += sentence
 
+    shaped_body = _format_two_more_lines(shaped_body)
+
     return shaped_body
 
 
-def _create_new_line_flag(body: str) -> str:
-    return re.sub(r'(\n|\r\n){2,}', "[NL]", body)
+def _create_sentence_block_flag(body: str) -> str:
+    return re.sub(r'(\n|\r\n){2,}', "[SB]", body)
 
 
 def _create_white_space_flag(body: str) -> str:
@@ -30,7 +32,7 @@ def _create_white_space_flag(body: str) -> str:
 
 
 def _create_colon_flag(body: str) -> str:
-    return re.sub(r'\w+:( |\n|\r\n)', ':[NL]', body)
+    return re.sub(r'(\w| ):( |\n|\r\n)+', ':[NL]', body)
 
 
 def _shape_new_line(sentence: str) -> str:
@@ -38,7 +40,9 @@ def _shape_new_line(sentence: str) -> str:
     shaped_sentence = _new_line_to_white_space(sentence)
 
     if re.search(r"\[NL\]", sentence):
-        shaped_sentence = shaped_sentence.replace("[NL]", "\n\n")
+        shaped_sentence = shaped_sentence.replace("[NL]", "\n")
+    elif re.search(r"\[SB\]", sentence):
+        shaped_sentence = shaped_sentence.replace("[SB]", "\n\n")
     else:
         shaped_sentence = shaped_sentence + "\n"
 
@@ -57,9 +61,14 @@ def _shape_white_space(sentence: str) -> str:
     return shaped_sentence
 
 
+# 文中で変な改行が含まれている時に空白に変換する
 def _new_line_to_white_space(sentence: str) -> str:
     return re.sub(r'((\n|\r\n)+)|( +(\n|\r\n)+)|((\n|\r\n)+ +)', ' ', sentence)
 
 
 def _split_body_to_sentences(body: str) -> List[str]:
     return sent_tokenize(body)
+
+# 文末の改行1個と[SB]の改行2個などで改行が3個以上発生する可能性があるのでその場合は2個に抑え込む
+def _format_two_more_lines(body: str) -> str:
+    return re.sub(r'((\n|\r\n){3,})', "\n\n", body)
