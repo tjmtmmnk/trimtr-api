7059a5d5a9175231113fc25e1ee1b90b3e0c33fc
tjmtmmnk <tsujimoto027@gmail.com>
2019-09-14 17:22:28 +0900

    Scoutを入れてみる

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from flask import request
 from flask import jsonify
 from flask_cors import CORS
 from trimtr.trimmer import Trimmer
+from scout_apm.flask import ScoutApm
 
 app = Flask(__name__)
 CORS(app)
+ScoutApm(app)
+
+app.config["SCOUT_MONITOR"] = os.environ.get("SCOUT_MONITOR")
+app.config["SCOUT_KEY"] = os.environ.get("SCOUT_KEY")
+app.config["SCOUT_NAME"] = "trimtr"
 
 
 @app.route('/', methods=['GET'])
